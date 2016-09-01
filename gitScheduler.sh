3f151a1f9b5c4ac4b75a1a371a7f88007ceb9939
skuarch <skuarch@yahoo.com.mx>
2016-09-01 16:05:12 -0500

    test 3

--- gitScheduler.sh
+++ gitScheduler.sh
@@ -5,11 +5,12 @@ USER="admin"
 PASSWORD="admin"
 HOST="localhost"
 PORT=8080
+UPDATED=false
 
 while true; do
     
-  [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-  sed 's/\// /g') | cut -f1) ] && echo "up to date" || echo "out of date"; git pull;
+  [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ] && UPDATED=true || UPDATED=false    
+  echo $UPDATED
   sleep 1;
    
 done
